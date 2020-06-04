c309c86313ff84730665033ab9a20afd166123ec
Cedric Patton <ceddyp117@gmail.com>
2020-06-04 21:07:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 function happyHolidays() { return `${"Happy holidays!"}`}
 
 
-function happyHolidaysTo(name) { return `${name}`}
+function happyHolidaysTo(name) { return `"Happy holidays,${name}!"`}
